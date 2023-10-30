f6918f928d578b0877adfba0fa43133e7c0b0110
«anonymous» <usuari@domini.com>
2023-10-30 12:11:29 +0100

    refreshed public api

--- projects/user-manager-structure-lib/src/public-api.ts
+++ projects/user-manager-structure-lib/src/public-api.ts
@@ -2,7 +2,30 @@
  * Public API Surface of user-manager-structure-lib
  */
 
+export * from './lib/models/application';
+export * from './lib/models/application-backend-service-role-id';
+export * from './lib/models/application-backend-service-role';
+export * from './lib/models/application-role';
+export * from './lib/models/application-role-id';
+export * from './lib/models/backend-service';
+export * from './lib/models/backend-service-role';
+export * from './lib/models/backend-service-role-id';
+export * from './lib/models/role';
+export * from './lib/models/basic-user';
+export * from './lib/models/granted-authority';
+export * from './lib/models/group';
+export * from './lib/models/check-credentials-request';
 export * from './lib/models/update-password-request';
+
+export * from './lib/services/application.service';
+export * from './lib/services/application-backend-service-role.service';
+export * from './lib/services/application-role.service';
 export * from './lib/services/auth.service';
-export * from './lib/services/user-manager-root.service';
+export * from './lib/services/backend-service-role.service';
+export * from './lib/services/basic-user.service';
+export * from './lib/services/caching.service';
+export * from './lib/services/group.service';
+export * from './lib/services/role.service';
 export * from './lib/services/session.service';
+export * from './lib/services/user.service';
+export * from './lib/services/user-manager-root.service';
