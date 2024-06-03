2ae3eaed63c0adf2888eedfed3cfef6ee3102eae
kbenlloch <kbenlloch@biit-solutions.com>
2024-06-03 12:53:21 +0200

    Added getAllByLoggedUser on OrganizationService

--- projects/user-manager-structure-lib/src/lib/services/organization.service.ts
+++ projects/user-manager-structure-lib/src/lib/services/organization.service.ts
@@ -48,6 +48,10 @@ export class OrganizationService {
     return this.httpClient.get<Organization[]>(
       `${this.rootService.serverUrl}${OrganizationService.ROOT_PATH}/users/${id}`);
   }
+  getAllByLoggedUser(): Observable<Organization[]> {
+    return this.httpClient.get<Organization[]>(
+      `${this.rootService.serverUrl}${OrganizationService.ROOT_PATH}/users`);
+  }
   getAllCreatedByCurrentUser(): Observable<Organization[]> {
     return this.httpClient.get<Organization[]>(
       `${this.rootService.serverUrl}${OrganizationService.ROOT_PATH}/createdBy`);
