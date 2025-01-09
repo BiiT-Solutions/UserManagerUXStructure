9886ff9d792dc5050f3de169e298da63697ed5a4
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-01-09 17:37:36 +0100

    Added public api

--- projects/user-manager-structure-lib/src/public-api.ts
+++ projects/user-manager-structure-lib/src/public-api.ts
@@ -19,6 +19,7 @@ export * from './lib/models/team';
 export * from './lib/models/user-group';
 export * from './lib/models/update-password-request';
 export * from './lib/models/signup-request';
+export * from './lib/utils/signup-request-converter';
 
 export * from './lib/services/application.service';
 export * from './lib/services/application-backend-service-role.service';
