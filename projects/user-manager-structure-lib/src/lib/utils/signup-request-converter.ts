82dae9832738b2618cd20c1bb72a31cb47ab1fab
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-01-24 11:31:24 +0100

    Allowing empty first name and last name

--- projects/user-manager-structure-lib/src/lib/utils/signup-request-converter.ts
+++ projects/user-manager-structure-lib/src/lib/utils/signup-request-converter.ts
@@ -12,14 +12,27 @@ export class SignupRequestConverter {
   }): SignUpRequest {
     const userManagerSignUpRequest: SignUpRequest = new SignUpRequest();
     if (!signUpRequest.username || !signUpRequest.username.length) {
-      userManagerSignUpRequest.username = `${signUpRequest.name[0]}${signUpRequest.lastname}${Math.trunc(Math.random() * 1000)}`
-        .replace(/[^a-zA-Z0-9]/g, '_');
+      if (signUpRequest.name && signUpRequest.name.length && signUpRequest.lastname && signUpRequest.lastname.length) {
+        userManagerSignUpRequest.username = `${signUpRequest.name[0]}${signUpRequest.lastname}${Math.trunc(Math.random() * 1000)}`
+          .replace(/[^a-zA-Z0-9]/g, '_');
+      } else {
+        userManagerSignUpRequest.username = signUpRequest.email;
+      }
     } else {
       userManagerSignUpRequest.username = signUpRequest.username;
     }
+    if (!signUpRequest.name || !signUpRequest.name.length) {
+      userManagerSignUpRequest.firstname = signUpRequest.name;
+    } else {
+      userManagerSignUpRequest.firstname = '--';
+    }
+    if (!signUpRequest.lastname || !signUpRequest.lastname.length) {
+      userManagerSignUpRequest.lastname = signUpRequest.lastname;
+    } else {
+      userManagerSignUpRequest.lastname = '--';
+    }
+
     userManagerSignUpRequest.email = signUpRequest.email;
-    userManagerSignUpRequest.firstname = signUpRequest.name;
-    userManagerSignUpRequest.lastname = signUpRequest.lastname;
     userManagerSignUpRequest.password = signUpRequest.password;
     userManagerSignUpRequest.team = signUpRequest.team;
     userManagerSignUpRequest.organization = signUpRequest.organization;
